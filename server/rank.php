bfd33e8f7590c72208ecdf8a16109a02fc99fc4c
changeforan <changeforan@gmail.com>
2015-03-17 00:08:50 +0800

    demo 1.5

---
+++ server/rank.php
@@ -0,0 +1,23 @@
+<?php
+$con = mysql_connect("localhost","root","1990911Root");
+if (!$con)
+{
+    die("Could not connect: " . mysql_error());
+}
+mysql_select_db("my_db_qia1s",$con);
+
+$request = $_POST["action"];
+
+if ($request == "insert") {
+
+    $name = $_POST["name"];
+    $points = $_POST["points"];
+    $title = $_POST["title"];
+    $sql = "insert into rank (name,points,title) VALUES ('$name','$points','$title')";
+    mysql_query($sql,$con);
+    echo("success");
+
+}else{
+
+}
+?>
